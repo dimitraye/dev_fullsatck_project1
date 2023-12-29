5c5da1be85e4c77fe581546359b7a86cb90ec3ef
dimitraye <dimitraye94@gmail.com>
2023-12-29 13:19:02 +0100

    unsubscribe

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Observable, of } from 'rxjs';
+import { Observable, Subscription, of } from 'rxjs';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 
 
@@ -10,11 +10,13 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
   styleUrls: ['./home.component.scss'],
 })
 
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   public olympics$: Observable<any> = of(null);
   numberOfJos! : number;
   numberOfCountries! : number;
   chartData : { name: string, value: number }[] = [];
+  private subscriptions: Subscription[] = [];
+
 
   constructor(private olympicService: OlympicService, private router: Router) {}
 
@@ -22,23 +24,33 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics();
-
-    this.olympicService.getNumberOfCountries().subscribe((count) => {
-      this.numberOfCountries = count;
-    });
-
-
-    this.olympicService.getOlympics().subscribe((olympics) => {
-      if (olympics) {
-        this.numberOfJos = olympics.length; //Nombre de jeux olympiques
-      }
-    });
-
-    
-    this.olympicService.getHomeChartData().subscribe((data) => {
-      this.chartData = data;
-    });
-  }
+  
+    this.subscriptions.push(
+      this.olympicService.getNumberOfCountries().subscribe((count) => {
+        this.numberOfCountries = count;
+      })
+    );
+  
+    this.subscriptions.push(
+      this.olympicService.getOlympics().subscribe((olympics) => {
+        if (olympics) {
+          this.numberOfJos = olympics.length; // Nombre de jeux olympiques
+        }
+      })
+    );
+  
+    this.subscriptions.push(
+      this.olympicService.getHomeChartData().subscribe((data) => {
+        this.chartData = data;
+      })
+    );
+    }
+  
+    ngOnDestroy(): void {
+    // Désinscrire toutes les souscriptions lors de la destruction du composant
+    this.subscriptions.forEach((subscription) => subscription.unsubscribe());
+    }
+  
 
   onSelect(data: { name: string; value: number }): void {
     this.router.navigateByUrl(`details/${data.name}`);
